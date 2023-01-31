cbac2a07483c5187d27445a15a98bb68b64e5c3e
Özkan ŞEN <ozkansen@gmail.com>
2023-01-31 17:56:20 +0300

    update login.go

--- 1_auto_scalable_user_login_service/cmd/handlers/login.go
+++ 1_auto_scalable_user_login_service/cmd/handlers/login.go
@@ -3,15 +3,20 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/ozkansen/experimental-projects/1_auto_scalable_user_login_service/pkg/encrypt"
 	"github.com/ozkansen/experimental-projects/1_auto_scalable_user_login_service/pkg/logger"
 )
 
 type Login struct {
-	log logger.Logger
+	log           logger.Logger
+	comparePasswd encrypt.Compare
 }
 
-func NewLoginHandler(l logger.Logger) *Login {
-	return &Login{log: l}
+func NewLoginHandler(c encrypt.Compare, l logger.Logger) *Login {
+	return &Login{
+		comparePasswd: c,
+		log:           l,
+	}
 }
 
 func (l *Login) Handler(w http.ResponseWriter, r *http.Request) {
