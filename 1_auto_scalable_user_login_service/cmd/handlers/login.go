0a19f89694b91560a096c3c897bb207a6813ea17
Özkan ŞEN <ozkansen@gmail.com>
2023-01-31 19:38:37 +0300

    add repo interface

--- 1_auto_scalable_user_login_service/cmd/handlers/login.go
+++ 1_auto_scalable_user_login_service/cmd/handlers/login.go
@@ -5,15 +5,18 @@ import (
 
 	"github.com/ozkansen/experimental-projects/1_auto_scalable_user_login_service/pkg/encrypt"
 	"github.com/ozkansen/experimental-projects/1_auto_scalable_user_login_service/pkg/logger"
+	"github.com/ozkansen/experimental-projects/1_auto_scalable_user_login_service/pkg/repository"
 )
 
 type Login struct {
 	log           logger.Logger
 	comparePasswd encrypt.Compare
+	repo          repository.Repository
 }
 
-func NewLoginHandler(c encrypt.Compare, l logger.Logger) *Login {
+func NewLoginHandler(r repository.Repository, c encrypt.Compare, l logger.Logger) *Login {
 	return &Login{
+		repo:          r,
 		comparePasswd: c,
 		log:           l,
 	}
