df7d80a473088b611cef3831bc289816e393d40f
Özkan ŞEN <ozkansen@gmail.com>
2023-01-31 14:36:59 +0300

    update main.go, logger implementation added

--- 1_auto_scalable_user_login_service/cmd/main.go
+++ 1_auto_scalable_user_login_service/cmd/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"github.com/ozkansen/experimental-projects/1_auto_scalable_user_login_service/pkg/logger"
+)
+
 func main() {
+	log := logger.NewStdLogger(logger.InfoLevel)
+	log.Info("starting user login api service")
+
+	if err := Run(log); err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+}
+
+func Run(log logger.Logger) error {
+	log.Info("running application service")
+	return nil
 }
