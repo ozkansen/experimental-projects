71f0799202acbb89737cb51cecf2cc1c30f6b8e1
Özkan ŞEN <ozkansen@gmail.com>
2023-01-31 23:16:13 +0300

    add handler

--- 1_user_login_api_service_scalable/cmd/handlers/login.go
+++ 1_user_login_api_service_scalable/cmd/handlers/login.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/ozkansen/experimental-projects/1_auto_scalable_user_login_service/pkg/encrypt"
@@ -22,6 +25,56 @@ func NewLoginHandler(r repository.Repository, c encrypt.Compare, l logger.Logger
 	}
 }
 
+type Request struct {
+	Username string `json:"username"`
+	Passwd   string `json:"passwd"`
+}
+
 func (l *Login) Handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("ok\n"))
+	if r.Method != http.MethodPost {
+		l.log.Warn("method not allowed")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	defer r.Body.Close()
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		l.log.Error(err.Error())
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var req Request
+	if err = json.Unmarshal(data, &req); err != nil {
+		l.log.Error(err.Error())
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	dbUser, err := l.repo.GetByUsername(context.Background(), req.Username)
+	if err != nil {
+		l.log.Error(err.Error())
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(data, &req); err != nil {
+		l.log.Error(err.Error())
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err = l.comparePasswd.Compare([]byte(dbUser.Passwd), []byte(req.Passwd)); err != nil {
+		l.log.Error(err.Error())
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Write([]byte("ok"))
 }
